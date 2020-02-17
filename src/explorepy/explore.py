bugfix, added calculation block to push2lsl

src/explorepy/explore.py
@@ -16,10 +16,11 @@ from threading import Thread, Timer
 
 class Explore:
     r"""Mentalab Explore device"""
-    def __init__(self, n_device=1):
+    def __init__(self, n_device=1, calibre_file=None):
         r"""
         Args:
             n_device (int): Number of devices to be connected
+            calibre_file (str): Calibration data file name
         """
         self.device = []
         self.socket = None
@@ -29,6 +30,7 @@ class Explore:
             self.device.append(BtClient())
         self.is_connected = False
         self.is_acquiring = None
+        self.calibration_file = calibre_file
 
     def connect(self, device_name=None, device_addr=None, device_id=0):
         r"""
@@ -45,7 +47,7 @@ class Explore:
         if self.socket is None:
             self.socket = self.device[device_id].bt_connect()
         if self.parser is None:
-            self.parser = Parser(socket=self.socket)
+            self.parser = Parser(socket=self.socket, calibration_file=self.calibration_file)
         self.is_connected = True
         packet = None
 
@@ -185,7 +187,7 @@ class Explore:
 
         assert self.is_connected, "Explore device is not connected. Please connect the device first."
 
-        info_orn = StreamInfo('Explore', 'Orientation', 9, 20, 'float32', 'ORN')
+        info_orn = StreamInfo('Explore', 'Orientation', 13, 20, 'float32', 'ORN')
         info_exg = StreamInfo('Explore', 'ExG', self.parser.n_chan, self.parser.fs, 'float32', 'ExG')
         info_marker = StreamInfo('Explore', 'Markers', 1, 0, 'int32', 'Marker')
 
@@ -223,20 +225,12 @@ class Explore:
             bp_freq (tuple): Bandpass filter cut-off frequencies (low_cutoff_freq, high_cutoff_freq), No bandpass filter
             if it is None.
             notch_freq (int): Line frequency for notch filter (50 or 60 Hz), No notch filter if it is None
-            calibre_file (str): Calibration data file name
         """
-        import numpy as np
         assert self.is_connected, "Explore device is not connected. Please connect the device first."
-        if calibre_file is not None:
-            with open(calibre_file, "r") as f_calibre:
-                csv_reader_calibre = csv.reader(f_calibre, delimiter=",")
-                calibre_set = list(csv_reader_calibre)
-                self.parser.calibre_set = np.asarray(calibre_set[1], dtype=np.float64)
         self.parser.notch_freq = notch_freq
         if bp_freq is not None:
             self.parser.apply_bp_filter = True
             self.parser.bp_freq = bp_freq
-
         self.m_dashboard = Dashboard(n_chan=self.parser.n_chan,
                                      exg_fs=self.parser.fs,
                                      firmware_version=self.parser.firmware_version)
@@ -249,43 +243,23 @@ class Explore:
 
     def _io_loop(self, device_id=0, mode="visualize"):
         self.is_acquiring = [True]
-        if self.parser.calibre_set is not None:
-            is_initialized = False
-        else:
-            is_initialized = True # flag as True since it doesn't matter and we skip orientation calculation process
         # Wait until dashboard is initialized.
         while not hasattr(self.m_dashboard, 'doc'):
             print('wait...')
             time.sleep(.5)
 
         while self.is_acquiring[0]:
-            if is_initialized:
+            try:
+                packet = self.parser.parse_packet(mode=mode, dashboard=self.m_dashboard)
+            except ConnectionAbortedError:
+                print("Device has been disconnected! Scanning for last connected device...")
                 try:
-                    packet = self.parser.parse_packet(mode=mode, dashboard=self.m_dashboard)
-                except ConnectionAbortedError:
-                    print("Device has been disconnected! Scanning for last connected device...")
-                    try:
-                        self.parser.socket = self.device[device_id].bt_connect()
-                    except DeviceNotFoundError as e:
-                        print(e)
-                        self.is_acquiring[0] = False
-                        if mode == "visualize":
-                            os._exit(0)
-            else:
-                try:
-                    packet = self.parser.parse_packet(mode="initialize", dashboard=self.m_dashboard)
-                    if hasattr(packet, 'acc'):
-                        if self.parser.init_set is not None:
-                            is_initialized = True
-                except ConnectionAbortedError:
-                    print("Device has been disconnected! Scanning for last connected device...")
-                    try:
-                        self.parser.socket = self.device[device_id].bt_connect()
-                    except DeviceNotFoundError as e:
-                        print(e)
-                        self.is_acquiring[0] = False
-                        if mode == "visualize":
-                            os._exit(0)
+                    self.parser.socket = self.device[device_id].bt_connect()
+                except DeviceNotFoundError as e:
+                    print(e)
+                    self.is_acquiring[0] = False
+                    if mode == "visualize":
+                        os._exit(0)
         os.exit(0)
 
     def signal_handler(self, signal, frame):

src/explorepy/packet.py
@@ -256,9 +256,9 @@ class Orientation(Packet):
         data = np.copy(np.frombuffer(bin_data, dtype=np.dtype(np.int16).newbyteorder('<'))).astype(np.float)
         self.acc = 0.061 * data[0:3]  # Unit [mg/LSB]
         self.gyro = 8.750 * data[3:6]  # Unit [mdps/LSB]
-        self.mag = 1.52 *  np.multiply (data[6:], np.array([-1, 1, 1]))  # Unit [mgauss/LSB]
-        self.theta = None
-        self.rot_axis = None
+        self.mag = 1.52 * np.multiply (data[6:], np.array([-1, 1, 1]))  # Unit [mgauss/LSB]
+        self.theta = 0
+        self.rot_axis = np.zeros(3, dtype=float)
 
     def _check_fletcher(self, fletcher):
         assert fletcher == b'\xaf\xbe\xad\xde', "Fletcher error!"
@@ -271,7 +271,8 @@ class Orientation(Packet):
                                      self.gyro.tolist() + self.mag.tolist())[:, np.newaxis])
 
     def push_to_lsl(self, outlet):
-        outlet.push_sample(self.acc.tolist() + self.gyro.tolist() + self.mag.tolist())
+        outlet.push_sample(self.acc.tolist() + self.gyro.tolist() + self.mag.tolist() +
+                           np.append(self.rot_axis, self.theta).tolist())
 
     def push_to_dashboard(self, dashboard):
         data = self.acc.tolist() + self.gyro.tolist() + self.mag.tolist()

src/explorepy/parser.py
@@ -6,6 +6,7 @@ from explorepy.packet import PACKET_ID, PACKET_CLASS_DICT, TimeStamp, EEG, Envir
 from explorepy.filters import Filter
 import copy
 import time
+import csv
 
 
 def generate_packet(pid, timestamp, bin_data):
@@ -30,7 +31,7 @@ def generate_packet(pid, timestamp, bin_data):
 
 
 class Parser:
-    def __init__(self, bp_freq=None, notch_freq=None, socket=None, fid=None):
+    def __init__(self, bp_freq=None, notch_freq=None, socket=None, fid=None, calibration_file=None):
         """Parser class for explore device
 
         Args:
@@ -38,6 +39,7 @@ class Parser:
             fid (file object): File object for reading data (Should be None if socket is provided)
             bp_freq (tuple): Tuple of cut-off frequencies of bandpass filter (low cut-off frequency, high cut-off frequency)
             notch_freq (int): Notch filter frequency (50 or 60 Hz)
+            calibration_file (str): Calibration data file name
         """
         self.socket = socket
         self.fid = fid
@@ -69,6 +71,19 @@ class Parser:
         packet = None
         while not isinstance(packet, DeviceInfo):
             packet = self.parse_packet(mode=None)
+        packet_counter = 0
+        if calibration_file is not None:
+            with open(calibration_file, "r") as f_calibre:
+                csv_reader_calibre = csv.reader(f_calibre, delimiter=",")
+                calibre_set = list(csv_reader_calibre)
+                self.calibre_set = np.asarray(calibre_set[1], dtype=np.float64)
+            while self.init_set is None:
+                self.parse_packet(mode="initialize")
+                packet_counter = packet_counter + 1
+                if packet_counter>15:
+                    print("Please make sure the orientation sensors are active. " +
+                          "Failed to initialize the orientation calculation block!")
+                    break
         self.signal_dc = np.zeros((self.n_chan,), dtype=np.float)
 
     @property
@@ -126,6 +141,8 @@ class Parser:
 
         elif mode == "lsl":
             if isinstance(packet, Orientation):
+                if self.init_set is not None:
+                    packet = self._compute_NED(packet)
                 packet.push_to_lsl(outlets[0])
             elif isinstance(packet, EEG):
                 packet.push_to_lsl(outlets[1])
@@ -144,7 +161,7 @@ class Parser:
                 #     self.signal_dc = (self.bp_freq[0] / (self.fs*0.5)) * packet.data[:, column] + (
                 #             1 - (self.bp_freq[0] / (self.fs*0.5))) * self.signal_dc
                 #     packet.data[:, column] = packet.data[:, column] - self.signal_dc
-            if isinstance(packet, Orientation) and self.calibre_set is not None:
+            if isinstance(packet, Orientation) and self.init_set is not None:
                 packet = self._compute_NED(packet)
             packet.push_to_dashboard(dashboard)
 